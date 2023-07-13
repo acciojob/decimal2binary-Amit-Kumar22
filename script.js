529c1d640d992f0cedbc48a130147ebb1bf83acf
Amit-Kumar22 <135856138+Amit-Kumar22@users.noreply.github.com>
2023-07-13 21:07:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,12 @@
-function decimalToBinary(decimal) {
+function decimalToBinary(a) {
   //Write you code here
-	// program to convert decimal to binary
-    while (decimal > 0) {
-   // if modulo of number with 2 is ‘1’, append 1 in front of binary string. Otherwise append 0.
-   if (decimal % 2 == 1) {
-      binary = "1" + binary;
-   } else {
-      binary = "0" + binary;
-   }
-   // divide number by 2.
-   decimal = Math.floor(decimal / 2);
-}
-    console.log(`Binary: ${bin}`);
+	var binary = '';
+  while (a>0)
+   {
+binary = (a%2) + binary;
+    a = Math.floor(a/2);
+     }
+returnbinary;
 }
 
 window.decimalToBinary = decimalToBinary;
